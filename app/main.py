7df92f05a7cdcb3aeefaa67a806e546b6fdc5643
Ganesh <ganesh.v.m.blr@gmail.com>
2026-06-04 06:34:38 +0000

    Add CI/CD pipeline

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-print("Hello from CI/CD pipeline")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from CI/CD pipeline"
+
+# This is the important part
+if __name__ == "__main__":
+    # 0.0.0.0 means "listen to all network interfaces"
+    # port=5000 means Flask runs on port 5000 inside the container
+    app.run(host="0.0.0.0", port=5000)
